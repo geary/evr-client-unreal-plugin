3590679292b555fb34c4124f18822c76a4ce36e4
Michael Geary <mg@mg.to>
2016-08-18 19:07:12 -0700

    Add tooltips

--- EasePlugin/Source/EasePlugin/Classes/EaseCameraComponent.h
+++ EasePlugin/Source/EasePlugin/Classes/EaseCameraComponent.h
@@ -14,21 +14,28 @@ UCLASS( MinimalAPI, meta = ( BlueprintSpawnableComponent ), ClassGroup = EASE )
 class UEaseCameraComponent : public UActorComponent {
 	GENERATED_UCLASS_BODY()
 
+	// Enter your API Key from the EASE dashboard
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	FString ApiKey;
 
+	// Enter your Experience ID from the EASE dashboard
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	FString ExperienceID;
 
+	// Check to enable the Presence API
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	bool EnablePresence;
 
+	// Check to log API calls to the Output Console
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	bool LogApiCalls;
 
+	// Check to log data pushes to the Output Console
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	bool LogPostData;
 
+	// Check to push live data to the EASE server,
+	// uncheck for local testing
 	UPROPERTY( EditAnywhere, Category = "EASE" )
 	bool EnablePostData;
 
